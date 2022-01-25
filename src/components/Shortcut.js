4f1cce9413725ad0fe3efa6047fedce799b1d33e
unknown <ditolord@gmail.com>
2022-01-25 19:43:51 +0700

    ini error di tambah data

--- src/components/Shortcut.js
+++ src/components/Shortcut.js
@@ -8,49 +8,49 @@ export default function Shortcut() {
     <div className="flex justify-around">
       <div className="text-center  w-20 h-30 shortcut-animate  mr-8">
         <a href="https://instagram.com">
-          <button id="instagram" class="shortcut-btn">
-            <div class="inner w-full h-full bg-gray-400 bg-opacity-30">
+          <button id="instagram" className="shortcut-btn">
+            <div className="inner w-full h-full bg-gray-400 bg-opacity-30">
               <img src={instagramlogo} className="" />
             </div>
           </button>
-          <label for="instagram" className="font-nauti block text-xl">
+          <label htmlFor="instagram" className="font-nauti block text-xl">
             Instagram
           </label>
         </a>
       </div>
       <div className="text-center  w-20 h-30 shortcut-animate  mr-4">
         <a href="https://google.com">
-          <button class="shortcut-btn">
-            <div class="inner w-full h-full bg-gray-400 bg-opacity-30">
+          <button className="shortcut-btn">
+            <div className="inner w-full h-full bg-gray-400 bg-opacity-30">
               <img src={googlelogo} className="" />
             </div>
           </button>
         </a>
-        <label for="instagram" className="font-nauti block text-xl">
+        <label htmlFor="instagram" className="font-nauti block text-xl">
           Google
         </label>
       </div>
       <div className="text-center  w-20 h-30 shortcut-animate  ml-4">
         <a href="https://youtube.com">
-          <button class="shortcut-btn">
-            <div class="inner w-full h-full bg-gray-400 bg-opacity-30">
+          <button className="shortcut-btn">
+            <div className="inner w-full h-full bg-gray-400 bg-opacity-30">
               <img src={youtubelogo} className="" />
             </div>
           </button>
         </a>
-        <label for="instagram" className="font-nauti block text-xl">
+        <label htmlFor="instagram" className="font-nauti block text-xl">
           Youtube
         </label>
       </div>
       <div className="text-center  w-20 h-30 shortcut-animate   ml-8">
         <a href="https://lms.unikom.ac.id">
-          <button class="shortcut-btn">
-            <div class="inner w-full h-full bg-gray-400 bg-opacity-30 p-2">
+          <button className="shortcut-btn">
+            <div className="inner w-full h-full bg-gray-400 bg-opacity-30 p-2">
               <img src={unikomlogo} className="" />
             </div>
           </button>
         </a>
-        <label for="instagram" className="font-nauti block text-xl">
+        <label htmlFor="instagram" className="font-nauti block text-xl">
           Lms Unikom
         </label>
       </div>
